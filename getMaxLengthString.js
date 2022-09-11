adc78af63395dd778af3656f9fc5522491a9f4ef
Wannarat Pumpipat <wannarat.pump@kmutt.ac.th>
2022-09-11 21:49:21 +0700

    getmaxlengthstring V4-passed

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -2,6 +2,10 @@ const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
 
+  if (arrayOfString === null || arrayOfString === undefined) {
+    return undefined
+  }
+
   let MaxLength = [arrayOfString[0]]
   
     for (let i = 1; i < arrayOfString.length; i++){
@@ -10,8 +14,6 @@ function getMaxLengthString(arrayOfString) {
     }
     else if(arrayOfString[i].length === MaxLength[0].length) {
       MaxLength.push(arrayOfString[i]) 
-    } if (arrayOfString === null || arrayOfString === undefined) {
-      return undefined
     }
   } 
   return MaxLength
